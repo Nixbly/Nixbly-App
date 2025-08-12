c5d0152081f3a7485967a82ba94648f580580b1e
NixblyApp <cesar@nixbly.com>
2025-08-12 00:43:40 -0600

    Add splash and onboarding screens (#2)

---
+++ NixblyApp/screens/SplashScreen.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+export default function SplashScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/splash-icon.png')} style={styles.image} />
+      <Text style={styles.title}>NixblyApp</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  image: {
+    width: 120,
+    height: 120,
+    marginBottom: 20,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
